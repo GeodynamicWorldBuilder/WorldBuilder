9d27e43f0ef87ec0ddc570d2687cb1e77d336cf4
MFraters <menno.fraters@outlook.com>
2018-07-05 11:00:14 +0200

    make the fortran wrapper into a module and clean a bit.

--- fortran/example.f90
+++ fortran/example.f90
@@ -1,81 +1,10 @@
 program test
-USE, INTRINSIC :: ISO_C_BINDING!, ONLY: C_PTR
-  IMPLICIT NONE
+use WorldBuilder
 
-  ! Create an interface with the create world function
-  INTERFACE
-    SUBROUTINE create_world(cworld, file_name) BIND(C, NAME='create_world')
-      USE, INTRINSIC :: ISO_C_BINDING, ONLY: C_PTR
-      IMPLICIT NONE
-      ! This argument is a pointer passed by reference.
-      TYPE(C_PTR), INTENT(OUT) :: cworld
-      character(len=1),  intent(in)  :: file_name
-    END SUBROUTINE create_world
-
-    ! Create an interface with the 2d tempearture function of the World builder
-    SUBROUTINE temperature_2d(cworld, x, z, depth, gravity, temperature) BIND(C, NAME='temperature_2d')
-      USE, INTRINSIC :: ISO_C_BINDING
-      IMPLICIT NONE
-      ! This argument is a pointer passed by value.
-      TYPE(C_PTR), INTENT(IN), VALUE :: cworld
-      REAL(C_DOUBLE), intent(in), value :: x
-      REAL(C_DOUBLE), intent(in), value :: z
-      REAL(C_DOUBLE), intent(in), value :: depth
-      REAL(C_DOUBLE), intent(in), value :: gravity
-      REAL(C_DOUBLE), intent(out) :: temperature
-    END SUBROUTINE temperature_2d
-
-    ! Create an interface with the 3d tempearture function of the World builder
-        SUBROUTINE temperature_3d(cworld, x, y, z, depth, gravity, temperature) BIND(C, NAME='temperature_3d')
-      USE, INTRINSIC :: ISO_C_BINDING
-      IMPLICIT NONE
-      ! This argument is a pointer passed by value.
-      TYPE(C_PTR), INTENT(IN), VALUE :: cworld
-      REAL(C_DOUBLE), intent(in), value :: x
-      REAL(C_DOUBLE), intent(in), value :: y
-      REAL(C_DOUBLE), intent(in), value :: z
-      REAL(C_DOUBLE), intent(in), value :: depth
-      REAL(C_DOUBLE), intent(in), value :: gravity
-      REAL(C_DOUBLE), intent(out) :: temperature
-    END SUBROUTINE temperature_3d
-
-    ! Create an interface with the 2d composition function of the World builder
-        SUBROUTINE composition_2d(cworld, x, z, depth, composition_number, composition) BIND(C, NAME='composition_2d')
-      USE, INTRINSIC :: ISO_C_BINDING
-      IMPLICIT NONE
-      ! This argument is a pointer passed by value.
-      TYPE(C_PTR), INTENT(IN), VALUE :: cworld
-      REAL(C_DOUBLE), intent(in), value :: x
-      REAL(C_DOUBLE), intent(in), value :: z
-      REAL(C_DOUBLE), intent(in), value :: depth
-      INTEGER(C_INT), intent(in), value :: composition_number
-      LOGICAL(C_BOOL), intent(out) :: composition
-    END SUBROUTINE composition_2d
-
-    ! Create an interface with the 3d composition function of the World builder
-      SUBROUTINE composition_3d(cworld, x, y, z, depth, composition_number, composition) BIND(C, NAME='composition_3d')
-      USE, INTRINSIC :: ISO_C_BINDING
-      IMPLICIT NONE
-      ! This argument is a pointer passed by value.
-      TYPE(C_PTR), INTENT(IN), VALUE :: cworld
-      REAL(C_DOUBLE), intent(in), value :: x
-      REAL(C_DOUBLE), intent(in), value :: y
-      REAL(C_DOUBLE), intent(in), value :: z
-      REAL(C_DOUBLE), intent(in), value :: depth
-      INTEGER(C_INT), intent(in), value :: composition_number
-      LOGICAL(C_BOOL), intent(out) :: composition
-    END SUBROUTINE composition_3d
-
-    ! Create an interface with the release world function.
-    SUBROUTINE release_world(cworld) BIND(C, NAME='release_world')
-      USE, INTRINSIC :: ISO_C_BINDING, ONLY: C_PTR
-      IMPLICIT NONE
-      ! This argument is a pointer passed by value.
-      TYPE(C_PTR), INTENT(IN), VALUE :: cworld
-    END SUBROUTINE release_world
-  END INTERFACE
+IMPLICIT NONE
 
   ! Declare the types which will be needed.
+
   TYPE(C_PTR) :: cworld
   REAL*8 :: temperature,x=120e3,y=500e3,z=0,depth=0,gravity = 10
   INTEGER :: composition_number = 3
--- source/wrapper_c.cc
+++ source/wrapper_c.cc
@@ -1,34 +1,6 @@
 #include <world_builder/world.h>
 #include <iostream>
-/*
-extern "C" void create_world(char* world_builder_file, void** world) {
-	printf("file is = %s\n",world_builder_file);
 
-	{
-		WorldBuilder::World* world_ptr = new WorldBuilder::World(std::string(world_builder_file));
-		std::cout << "pointer 1= " << world << std::endl;
-		*world = reinterpret_cast<void*>(world_ptr);
-	}
-
-	std::cout << "pointer 2 = " << world << std::endl;
-}
-
-extern "C" void release_world(void* world) {
-	std::cout << "pointer 4 = " << world << ", deref = " << &world << std::endl;
-	WorldBuilder::World* world_ptr = reinterpret_cast<WorldBuilder::World*>(world);
-	         delete world_ptr;
-	//delete static_cast<WorldBuilder::World*> (world);
-}
-
-extern "C" void temperature_2d(void* world)//, double* temperature)//double x, double z, double depth, double gravity, double temperature)
-{
-	std::cout << "pointer 3 = " << world << ", deref = " << &world << std::endl;
-	WorldBuilder::World* world_ptr = reinterpret_cast<WorldBuilder::World*>(world);
-	std::array<double,2> position = {1,2};
-	double temperature = world_ptr->temperature(position,3,4);
-	std::cout << " temp = " << temperature << std::endl;
-}
-*/
 extern "C" {
     /**
      * This function creates an object of the world builder and returns a pointer
