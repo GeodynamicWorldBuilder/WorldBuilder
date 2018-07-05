f280d503fbba3af071605be2cd96a70c8f9cb636
MFraters <menno.fraters@outlook.com>
2018-07-05 18:00:48 +0200

    Add initial tryout of the tester.

---
+++ tests/boost_tester.cc
@@ -0,0 +1,9 @@
+#define BOOST_TEST_MAIN
+#if !defined( WIN32 )
+    #define BOOST_TEST_DYN_LINK
+#endif
+#include <boost/test/unit_test.hpp>
+
+BOOST_AUTO_TEST_CASE( la ) {
+    BOOST_CHECK_EQUAL(1, 1);
+}
