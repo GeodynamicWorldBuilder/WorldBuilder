3e44114830a19b80f747bcf9e25d8bd469640473
MFraters <menno.fraters@outlook.com>
2018-07-05 18:33:55 +0200

    add first test.

--- tests/boost_tester.cc
+++ tests/boost_tester.cc
@@ -2,8 +2,13 @@
 #if !defined( WIN32 )
     #define BOOST_TEST_DYN_LINK
 #endif
+#define BOOST_TEST_MODULE "BaseClassModule"
 #include <boost/test/unit_test.hpp>
 
 BOOST_AUTO_TEST_CASE( la ) {
     BOOST_CHECK_EQUAL(1, 1);
+}
+BOOST_AUTO_TEST_CASE( lb ) {
+    BOOST_CHECK_EQUAL(1, 1);
+    BOOST_CHECK_EQUAL(2, 2);
 }
