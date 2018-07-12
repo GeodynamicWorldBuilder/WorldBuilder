9a1722ed670a5afb778aa7b5c1a1cdda0d2ee767
MFraters <menno.fraters@outlook.com>
2018-07-12 19:19:38 +0200

    forgot to add new files.

---
+++ include/world_builder/types/unsigned_int.h
@@ -0,0 +1,78 @@
+/*
+  Copyright (C) 2018 by the authors of the World Builder code.
+
+  This file is part of the World Builder.
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU Lesser General Public License as published
+   by the Free Software Foundation, either version 2 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public License
+   along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#ifndef _world_feature_types_unsigned_int_h
+#define _world_feature_types_unsigned_int_h
+
+#include <world_builder/types/interface.h>
+
+
+namespace WorldBuilder
+{
+  namespace Types
+  {
+
+    /**
+     * This class represents a continental plate and can implement submodules
+     * for temperature and composition. These submodules determine what
+     * the returned temperature or composition of the temperature and composition
+     * functions of this class will be.
+     */
+    class UnsignedInt : public Interface
+    {
+      public:
+        /**
+         * A constructor for the load_entry function
+         */
+        UnsignedInt(unsigned int default_value, std::string description);
+
+        /**
+         * A constructor for the clone and set_entry function
+         */
+        UnsignedInt(unsigned int value, unsigned int default_value, std::string description);
+
+        /**
+         * Destructor
+         */
+        ~UnsignedInt();
+
+        /**
+         * Clone. The caller of clone is responsible for the lifetime of it,
+         * so return a unique pionter.
+         */
+        virtual
+        std::unique_ptr<Interface> clone() const;
+
+        /**
+         * Set value
+         */
+        virtual
+        void set_value(unsigned int value);
+
+        unsigned int value;
+        unsigned int default_value;
+        std::string description;
+
+      private:
+
+    };
+  }
+}
+
+#endif
---
+++ source/types/unsigned_int.cc
@@ -0,0 +1,62 @@
+/*
+  Copyright (C) 2018 by the authors of the World Builder code.
+
+  This file is part of the World Builder.
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU Lesser General Public License as published
+   by the Free Software Foundation, either version 2 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public License
+   along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+#include <world_builder/types/unsigned_int.h>
+#include <world_builder/assert.h>
+#include <world_builder/utilities.h>
+
+namespace WorldBuilder
+{
+  namespace Types
+  {
+    UnsignedInt::UnsignedInt(unsigned int default_value, std::string description)
+      :
+      value(default_value),
+      default_value(default_value),
+      description(description)
+    {
+      this->type_name = type::UnsignedInt;
+    }
+
+    UnsignedInt::UnsignedInt(unsigned int value, unsigned int default_value, std::string description)
+      :
+      value(value),
+      default_value(default_value),
+      description(description)
+    {
+      this->type_name = type::UnsignedInt;
+
+    }
+
+    UnsignedInt::~UnsignedInt ()
+    {}
+
+    std::unique_ptr<Interface>
+    UnsignedInt::clone() const
+    {
+      return std::unique_ptr<Interface>(new UnsignedInt(value, default_value, description));
+    }
+
+    void
+    UnsignedInt::set_value(unsigned int value_)
+    {
+      value = value_;
+    }
+  }
+}
+
