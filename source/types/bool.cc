eb736db33a7c6b1c39f5840b9e362f74fab1dbf8
MFraters <menno.fraters@outlook.com>
2018-11-22 21:40:01 +0100

    Als add new types object and bool.

---
+++ source/types/bool.cc
@@ -0,0 +1,91 @@
+/*
+  Copyright (C) 2018 by the authors of the World Builder code.
+
+  This file is part of the World Builder.
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU Lesser General Public License as published
+   by the Free Software Foundation, either version 2 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public License
+   along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+#include <world_builder/types/bool.h>
+#include <world_builder/assert.h>
+#include <world_builder/utilities.h>
+#include <world_builder/parameters.h>
+
+namespace WorldBuilder
+{
+  namespace Types
+  {
+    Bool::Bool()
+      :
+      value(false),
+      default_value(false)
+    {
+      this->type_name = Types::type::Bool;
+    }
+
+
+    Bool::Bool(bool value, bool default_value)
+      :
+      value(value),
+      default_value(default_value)
+    {
+      this->type_name = Types::type::Bool;
+    }
+
+
+    Bool::~Bool ()
+    {}
+
+    std::unique_ptr<Interface>
+    Bool::clone() const
+    {
+      return std::unique_ptr<Interface>(new Bool(value, default_value));
+    }
+
+    void
+    Bool::write_schema(Parameters &prm,
+                       const std::string name,
+                       const std::string default_value,
+                       const bool required,
+                       const std::string documentation) const
+    {
+      using namespace rapidjson;
+      Document &declarations = prm.declarations;
+      const std::string path = prm.get_full_json_path();
+      const std::string type_name = "boolean";
+      //Pointer((path + "/type").c_str()).Set(declarations,"object");
+      const std::string base = path + "/" + name;
+      //std::cout << "base name = " << base << std::endl;
+      Pointer((base + "/default value").c_str()).Set(declarations,default_value == "true" ? true : false);
+      Pointer((base + "/required").c_str()).Set(declarations,required);
+      Pointer((base + "/type").c_str()).Set(declarations,type_name.c_str());
+      Pointer((base + "/documentation").c_str()).Set(declarations,documentation.c_str());
+      /*if (required)
+        {
+          if (Pointer((path + "/required").c_str()).Get(declarations) == NULL)
+            {
+              // The required array doesn't exist yet, so we create it and fill it.
+              Pointer((path + "/required/0").c_str()).Create(declarations);
+              Pointer((path + "/required/0").c_str()).Set(declarations, name.c_str());
+            }
+          else
+            {
+              // The required array already exist yet, so we add an element to the end.
+              Pointer((path + "/required/-").c_str()).Set(declarations, name.c_str());
+            }
+        }*/
+    }
+
+  }
+}
+
