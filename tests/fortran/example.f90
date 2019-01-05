5c60dbdd00ca7da3df50f77727481bd777ce3886
MFraters <menno.fraters@outlook.com>
2019-01-05 01:01:31 +0100

    fix fortran tests.

--- tests/fortran/example.f90
+++ tests/fortran/example.f90
@@ -7,12 +7,16 @@ IMPLICIT NONE
   REAL*8 :: temperature,x=120e3,y=500e3,z=0,depth=0,gravity = 10
   INTEGER :: composition_number = 3
   REAL*8 :: composition
-  character(len=256) :: file_name = "../../../tests/data/continental_plate.wb"//C_NULL_CHAR
+  !character(len=256) :: path
+  INTEGER*4 :: k = 1
+  character(len=256) :: file_name != MY_FLAG//"/data/continental_plate.wb"//C_NULL_CHAR
   logical(1) :: has_output_dir = .false.
   character(len=256) :: output_dir = "../../../doc/manual/"//C_NULL_CHAR
 
+  call getarg( k, file_name )
+!  file_name = trim(file_name//C_NULL_CHAR
   ! Show how to call the functions.
-  CALL create_world(cworld, file_name, has_output_dir, output_dir)
+  CALL create_world(cworld, trim(file_name)//C_NULL_CHAR, has_output_dir, output_dir)
 
   write(*, *) '2d temperature:'
   CALL temperature_2d(cworld,x,z,depth,gravity,temperature)
