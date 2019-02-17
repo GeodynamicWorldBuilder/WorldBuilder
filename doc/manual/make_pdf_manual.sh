77d764c5b9d2014aaab48171ae90f28d3caf7435
MFraters <menno.fraters@outlook.com>
2019-02-16 20:31:49 -0800

    make sure pdf directory exists.

--- doc/manual/make_pdf_manual.sh
+++ doc/manual/make_pdf_manual.sh
@@ -1,1 +1,5 @@
-pdflatex -output-directory pdf -interaction=batchmode manual.tex && bibtex pdf/manual && pdflatex -output-directory pdf -interaction=batchmode manual.tex && pdflatex -output-directory pdf -interaction=batchmode manual.tex
+mkdir -p pdf && \
+pdflatex -output-directory pdf -interaction=batchmode manual.tex && \
+bibtex pdf/manual && \
+pdflatex -output-directory pdf -interaction=batchmode manual.tex && \
+pdflatex -output-directory pdf -interaction=batchmode manual.tex
